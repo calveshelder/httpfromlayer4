4e2b1b5a0814119e93a44640f1222109f6c21f89
Helder Gonçalves <goncalves.helder@proton.me>
2026-01-23 22:00:43 +0000

    feat: requests with tests

---
+++ internal/request/request.go
@@ -0,0 +1,69 @@
+package request
+
+import (
+	"errors"
+	"io"
+	"log"
+	"strings"
+	"unicode"
+)
+
+type Request struct {
+	RequestLine RequestLine
+}
+
+type RequestLine struct {
+	HttpVersion   string
+	RequestTarget string
+	Method        string
+}
+
+func RequestFromReader(reader io.Reader) (*Request, error) {
+	request, err := io.ReadAll(reader)
+	if err != nil {
+		log.Fatal(err)
+	}
+	requestString := string(request)
+	requestStringParts := strings.Split(requestString, "\r\n")
+	firstLine := requestStringParts[0]
+	parsedRequestLine, err := parseRequestLine(firstLine)
+	if err != nil {
+		return nil, err
+	}
+
+	req := &Request{
+		RequestLine: parsedRequestLine,
+	}
+
+	return req, nil
+}
+
+func parseRequestLine(firstLine string) (RequestLine, error) {
+	parts := strings.Split(firstLine, " ")
+	if len(parts) != 3 {
+		return RequestLine{}, errors.New("Invalid string")
+	}
+
+	method := parts[0]
+	target := parts[1]
+	versionNumber := strings.Split(parts[2], "/")
+	number := versionNumber[1]
+
+	for _, v := range parts[0] {
+		isLetter := unicode.IsLetter(v)
+		isUpper := unicode.IsUpper(v)
+		isValid := isLetter && isUpper
+
+		if !isValid {
+			return RequestLine{}, errors.New("Method is not valid")
+		}
+	}
+
+	rl := RequestLine{
+		Method:        method,
+		RequestTarget: target,
+		HttpVersion:   number,
+	}
+
+	return rl, nil
+}
